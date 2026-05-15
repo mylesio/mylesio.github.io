16c441475d0e086f46eff5ee7777b28a43f3bd77
myles <boyce51532853@gmail.com>
2026-05-15 13:36:49 +0800

    fix: strip integrations, minimal config to unblock build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://mylesio.github.io',
-  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
